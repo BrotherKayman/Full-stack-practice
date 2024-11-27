02f888516ce63ffd44a19a58b323d704169e0ecc
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-11-28 01:34:44 +0200

    upgrade mouse event

--- Websites/scripts.js
+++ Websites/scripts.js
@@ -41,3 +41,37 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 
+//mouse cursor effect
+
+let mouseCursor = document.querySelector('.cursor'); 
+let info_nav = document.querySelectorAll('.info-nav ul');
+let reviews = document.querySelector('.reviews');
+let below_body = document.querySelector('.below-body');
+let doctors = document.querySelector('.doctors');
+let about_us = document.querySelector('about-us');
+let benefits = document.querySelector('.benefits');
+let hero_section = document.querySelector('hero-section');
+let hero_right = document.querySelector('.right-side');
+let hero_left = document.querySelector('.left-side');
+
+window.addEventListener('mousemove', cursor);
+
+function cursor(e){
+  mouseCursor.style.top = e.pageY + 'px'; //px = pixels
+  mouseCursor.style.left = e.pageX + 'px';
+}
+
+info_nav.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('link-grow');
+  });
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('link-grow');
+  });
+});
+
+hero_section.forEach(ul =>{
+link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('link-grow');
+  });
+});
